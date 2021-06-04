013025ea211947d03aa1632daefbd53152877748
siuyan <aliachmadramadhan@gmail.com>
2021-06-04 13:47:26 +0700

    Update index.blade.php

--- resources/views/alternatif_nilai/index.blade.php
+++ resources/views/alternatif_nilai/index.blade.php
@@ -48,4 +48,49 @@
     </div>
 </div>
 </div>
+<!--
+ <button type="button" class="btn btn-primary mr-2" data-toggle="modal" data-target="#Modal{{$item->alternatif_id}}">
+                                     Edit
+                                 </button>
+                <div class="modal fade" id="Modal{{$item->alternatif_id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                  <div class="modal-dialog modal-dialog-centered" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLongTitle">Alternatif 
+                            <span class="font-weight-bold">{{ $item->alternatif_nama }}</span></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+                  <div class="modal-body px-4">
+                    <div class="row">
+                     <div class="form-group col">
+                        <label for="exampleFormControlInput1">JAK</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="fill number of criteria">
+                    </div>
+                     <div class="form-group col">
+                        <label for="exampleFormControlInput1">JAS</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="fill number of criteria">
+                    </div>
+                    </div>
+
+                    <div class="row">
+                     <div class="form-group col">
+                        <label for="exampleFormControlInput1">JAB</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="fill number of criteria">
+                    </div>
+                     <div class="form-group col">
+                        <label for="exampleFormControlInput1">JKK</label>
+                        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="fill number of criteria">
+                    </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+-->
 @endsection
